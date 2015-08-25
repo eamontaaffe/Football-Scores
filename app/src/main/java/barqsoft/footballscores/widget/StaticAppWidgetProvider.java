ab16945d0aa04ec42e774f00c9c8e84581ec9e71
eamon <eamontaaffe@gmail.com>
2015-08-25 17:40:11 +1000

    not showing up yet

---
+++ app/src/main/java/barqsoft/footballscores/widget/StaticAppWidgetProvider.java
@@ -0,0 +1,15 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by Eamon on 25/08/2015.
+ */
+public class StaticAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
